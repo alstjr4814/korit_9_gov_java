094be06a8a3bc39735ed3ed7751f382c69719472
code1218 <skjil1218@gmail.com>
2025-10-24 15:51:08 +0900

    builder

--- src/main/java/com/korit/study/ch27/BuilderMain.java
+++ src/main/java/com/korit/study/ch27/BuilderMain.java
@@ -4,10 +4,21 @@ public class BuilderMain {
     public static void main(String[] args) {
         BuilderStudy builderStudy = new BuilderStudy("김준일", 32);
         BuilderStudy builderStudy1 = BuilderStudy.builder()
+                .value1("김준일")
                 .value2(32)
                 .build();
 
-//        Product product = Product.builder.name("상품1").price(1000).size("L").build();
+        ComputerBuilder computerBuilder = Computer.builder();
+        computerBuilder.setCpu("i7");
+        computerBuilder.setRam(8);
+        Computer computer = computerBuilder.build();
+
+        ComputerBuilder computerBuilder2 = Computer.builder().cpu("i5").ram(16);
+        Computer computer2 = computerBuilder2.build();
+
+        Computer computer3 = Computer.builder().ram(16).cpu("i5").build();
+
+//        Product product = Product.builder().name("상품1").price(1000).size("L").build();
 //        System.out.println(product);
     }
 }
--- src/main/java/com/korit/study/ch27/Product.java
+++ src/main/java/com/korit/study/ch27/Product.java
@@ -4,4 +4,11 @@ public class Product {
     private String name;
     private Integer price;
     private String size;
+
+    public Product(String name, Integer price, String size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
 }
